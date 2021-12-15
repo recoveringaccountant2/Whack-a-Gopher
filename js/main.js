4ba58d5b9ebb6d99c8b8e392963ea885355a3ab3
Your Name <charlesjohnstoncpa@gmail.com>
2021-12-15 12:20:34 -0700

    am push

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 ////////// audio //////////
 
-// probably won't use all of these.  anything I could find on a sound board.  
+// probably won't use all of these.  anything funny I could find on a sound board.  
 
 const welcome = new Audio('audio/Theme.mp3');
 const killGophers = new Audio('audio/kill-all-the-gophers.mp3');
@@ -89,35 +89,75 @@ function screenStyleTransitionOneTwo() {
 
 // define character options
 
-const character = ['Ty Webb', 'Lacey Underall', `Maggie O'Hooligan`, 'Al Czervik', 'Judge Elihu Smails', 'Carl Spackler', 'Mrs. Smails', 'Danny Noonan'];
+const character = [
+    {name: 'Ty Webb', photoUrl: 'imgs/ty2.jpeg'},
+    {name: 'Lacey Underall', photoUrl: 'imgs/lacey.png'},
+    {name: `Maggie O'Hooligan`, photoUrl: 'imgs/maggie.jpeg'},
+    {name: 'Al Czervik', photoUrl: 'imgs/danger2.jpeg'},
+    {name: 'Judge Elihu Smails', photoUrl: 'imgs/judge.png'},
+    {name: 'Carl Spackler', photoUrl: 'imgs/carl2.png'},
+    {name: 'Mrs. Smails', photoUrl: 'imgs/mrssmails.jpeg'},
+    {name: 'Danny Noonan', photoUrl: 'imgs/danny.jpeg'},
+];
 
 // create empty element to hold character selected
 
 let characterSelected = null
+
+
+
+function clearCharacterSelection() {
+    // clear frame from any previously selected characters
+    document.getElementById('ty').style.border = 'none';
+    document.getElementById('lacey').style.border = 'none';
+    document.getElementById('maggie').style.border = 'none';
+    document.getElementById('al').style.border = 'none';
+    document.getElementById('judge').style.border = 'none';
+    document.getElementById('carl').style.border = 'none';
+    document.getElementById('mrssmails').style.border = 'none';
+    document.getElementById('danny').style.border = 'none';
+
+    // turn off and reset any previous audio (except theme song)
+    likeYouBetty.pause();
+    likeYouBetty.currentTime = 0;
+    freezeGopher.pause();
+    freezeGopher.currentTime = 0;
+    thanksForNothing.pause();
+    thanksForNothing.currentTime = 0;
+    hat.pause();
+    hat.currentTime = 0;
+    areYouMyPal.pause();
+    areYouMyPal.currentTime = 0;
+    gungaShort.pause();
+    gungaShort.currentTime = 0;
+    thankYouVeryLittle.pause();
+    thankYouVeryLittle.currentTime = 0;
+    beTheBall.pause();
+    beTheBall.currentTime = 0;
+
+    // clear border
+
+    // document.getElementByClassName('character').style.border = 'none';
+    // document.getElementsByClassName('character').style.border = 'none';
+    // document.querySelector('character').style.border = 'none';
+    // document.querySelectorAll('character').style.border = 'none';
+
+
+}
+
 
 // event listener for Ty
 document.getElementById('ty').addEventListener('click', selectTy);
 
 function selectTy() {
     // clear frame from any previously selected characters
-    document.getElementById('ty').style.border = '0px solid white';
-    document.getElementById('lacey').style.border = '0px solid white';
-    document.getElementById('maggie').style.border = '0px solid white';
-    document.getElementById('al').style.border = '0px solid white';
-    document.getElementById('judge').style.border = '0px solid white';
-    document.getElementById('carl').style.border = '0px solid white';
-    document.getElementById('mrssmails').style.border = '0px solid white';
-    document.getElementById('danny').style.border = '0px solid white';
-
+    clearCharacterSelection();
     // add frame to selected character for visual confirmation of selection
     document.getElementById('ty').style.border = '2px solid white';
-
     // cue audio file for character
     likeYouBetty.play();
-
     // volumne set above theme music level
     likeYouBetty.volume = .8;    
-
     characterSelected = 0;
 }
 
@@ -125,20 +165,10 @@ function selectTy() {
 document.getElementById('lacey').addEventListener('click', selectLacey);
 
 function selectLacey() {
-    document.getElementById('ty').style.border = '0px solid white';
-    document.getElementById('lacey').style.border = '0px solid white';
-    document.getElementById('maggie').style.border = '0px solid white';
-    document.getElementById('al').style.border = '0px solid white';
-    document.getElementById('judge').style.border = '0px solid white';
-    document.getElementById('carl').style.border = '0px solid white';
-    document.getElementById('mrssmails').style.border = '0px solid white';
-    document.getElementById('danny').style.border = '0px solid white';
-
+    clearCharacterSelection();
     document.getElementById('lacey').style.border = '2px solid white';
-
     freezeGopher.play();
     freezeGopher.volume = .8;    
-
     characterSelected = 1;
 }
 
@@ -146,20 +176,10 @@ function selectLacey() {
 document.getElementById('maggie').addEventListener('click', selectMaggie);
 
 function selectMaggie() {
-    document.getElementById('ty').style.border = '0px solid white';
-    document.getElementById('lacey').style.border = '0px solid white';
-    document.getElementById('maggie').style.border = '0px solid white';
-    document.getElementById('al').style.border = '0px solid white';
-    document.getElementById('judge').style.border = '0px solid white';
-    document.getElementById('carl').style.border = '0px solid white';
-    document.getElementById('mrssmails').style.border = '0px solid white';
-    document.getElementById('danny').style.border = '0px solid white';
-
+    clearCharacterSelection();
     document.getElementById('maggie').style.border = '2px solid white';
-
     thanksForNothing.play();
     thanksForNothing.volume = .8;
-
     characterSelected = 2;
 }
 
@@ -167,20 +187,10 @@ function selectMaggie() {
 document.getElementById('al').addEventListener('click', selectAl);
 
 function selectAl() {
-    document.getElementById('ty').style.border = '0px solid white';
-    document.getElementById('lacey').style.border = '0px solid white';
-    document.getElementById('maggie').style.border = '0px solid white';
-    document.getElementById('al').style.border = '0px solid white';
-    document.getElementById('judge').style.border = '0px solid white';
-    document.getElementById('carl').style.border = '0px solid white';
-    document.getElementById('mrssmails').style.border = '0px solid white';
-    document.getElementById('danny').style.border = '0px solid white';
-
+    clearCharacterSelection();
     document.getElementById('al').style.border = '2px solid white';
-
     hat.play();
     hat.volume = .8;
-
     characterSelected = 3;
 }
 
@@ -188,20 +198,10 @@ function selectAl() {
 document.getElementById('judge').addEventListener('click', selectJudge);
 
 function selectJudge() {
-    document.getElementById('ty').style.border = '0px solid white';
-    document.getElementById('lacey').style.border = '0px solid white';
-    document.getElementById('maggie').style.border = '0px solid white';
-    document.getElementById('al').style.border = '0px solid white';
-    document.getElementById('judge').style.border = '0px solid white';
-    document.getElementById('carl').style.border = '0px solid white';
-    document.getElementById('mrssmails').style.border = '0px solid white';
-    document.getElementById('danny').style.border = '0px solid white';
-
+    clearCharacterSelection();
     document.getElementById('judge').style.border = '2px solid white';
-
     areYouMyPal.play();
     areYouMyPal.volume = .8;
-
     characterSelected = 4;
 }
 
@@ -209,20 +209,10 @@ function selectJudge() {
 document.getElementById('carl').addEventListener('click', selectCarl);
 
 function selectCarl() {
-    document.getElementById('ty').style.border = '0px solid white';
-    document.getElementById('lacey').style.border = '0px solid white';
-    document.getElementById('maggie').style.border = '0px solid white';
-    document.getElementById('al').style.border = '0px solid white';
-    document.getElementById('judge').style.border = '0px solid white';
-    document.getElementById('carl').style.border = '0px solid white';
-    document.getElementById('mrssmails').style.border = '0px solid white';
-    document.getElementById('danny').style.border = '0px solid white';
-
+    clearCharacterSelection();
     document.getElementById('carl').style.border = '2px solid white';
-
     gungaShort.play();
     gungaShort.volume = .8;
-
     characterSelected = 5;
 }
 
@@ -230,20 +220,10 @@ function selectCarl() {
 document.getElementById('mrssmails').addEventListener('click', selectMrsSmails);
 
 function selectMrsSmails() {
-    document.getElementById('ty').style.border = '0px solid white';
-    document.getElementById('lacey').style.border = '0px solid white';
-    document.getElementById('maggie').style.border = '0px solid white';
-    document.getElementById('al').style.border = '0px solid white';
-    document.getElementById('judge').style.border = '0px solid white';
-    document.getElementById('carl').style.border = '0px solid white';
-    document.getElementById('mrssmails').style.border = '0px solid white';
-    document.getElementById('danny').style.border = '0px solid white';
-
+    clearCharacterSelection();
     document.getElementById('mrssmails').style.border = '2px solid white';
-
     thankYouVeryLittle.play();
     thankYouVeryLittle.volume = .8;
-
     characterSelected = 6;
 }
 
@@ -251,20 +231,10 @@ function selectMrsSmails() {
 document.getElementById('danny').addEventListener('click', selectDanny);
 
 function selectDanny() {
-    document.getElementById('ty').style.border = '0px solid white';
-    document.getElementById('lacey').style.border = '0px solid white';
-    document.getElementById('maggie').style.border = '0px solid white';
-    document.getElementById('al').style.border = '0px solid white';
-    document.getElementById('judge').style.border = '0px solid white';
-    document.getElementById('carl').style.border = '0px solid white';
-    document.getElementById('mrssmails').style.border = '0px solid white';
-    document.getElementById('danny').style.border = '0px solid white';
-
+    clearCharacterSelection();
     document.getElementById('danny').style.border = '2px solid white';
-
     beTheBall.play();
     beTheBall.volume = .8;
-
     characterSelected = 7;
 }
 
@@ -278,8 +248,10 @@ function initializeGame() {
     // play "kill all the golfers/gophers" dialogue
     killGophers.play();
     killGophers.volume = .8;
-    // set player name variable
+    // set value of player name variable
     playerName = document.querySelector('input').value;
+    // player name to title on game screen
+    document.getElementById('player-name-scoreboard').innerText = playerName;
     // style transition page 2 --> page 3
     screenStyleTransitionTwoThree()
 
@@ -322,6 +294,16 @@ function screenStyleTransitionTwoThree() {
 
     document.getElementById('game-box').addEventListener('hover', hammerCursor);
 
+
+
+
+
+    playerNameToScoreboard (playerName);
+
+
+    document.getElementById('player-picture').src = character[characterSelected].photoUrl;
+
+
 }
 
 
@@ -336,9 +318,139 @@ document.getElementById('game-box').style.cursor = 'imgs/hammercursor.png';
 
 
 
-
-
 ////////// screen 3 //////////
+
+
+
+
+
+
+
+
+// define game board variables 
+let playerScore = 0;
+let gopherScore = 0;
+let gameDifficulty = 1;
+const gameActive = false;
+const gameConcluded = false;
+const timeRemaining = 60;
+
+const results = [];
+
+// previously defined:
+// let playerName
+// const character
+
+
+
+// ?? it would be cool to have an aviation click knob like on a 1073
+
+
+const holesEl = document.querySelectorAll('.hole');
+
+const gophersEl = document.querySelectorAll('.gopher');
+
+const playerNameEl = document.getElementById('player-name-scoreboard');
+
+
+// display player name on game screen
+
+function playerNameToScoreboard (){
+    document.getElementById('player-name-scoreboard').innerText = playerName;
+    document.getElementById('player-name-scoreboard-as').innerText = `${character[characterSelected].name}`
+}
+
+const difficultyOptions = ['slow poke', `lil' less embarrassing`, 'Speedy McClickerson', 'ludicrouse', 'plaid']
+
+
+// const gameDifficulty = 1;
+
+
+// event listener and function to set gameDifficulty
+document.getElementById('difficulty-input').addEventListener('click', updateDifficulty)
+
+
+function updateDifficulty (){
+    gameDifficulty = document.getElementById('difficulty-input').value
+    document.getElementById('difficulty-selected').innerText = difficultyOptions[gameDifficulty];
+}
+
+
+
+
+
+
+// get gopher to display
+
+
+
+
+// begin game
+function start (){
+
+
+
+
+    // let playerScore = 0;
+    // let gopherScore = 0;
+    // let gameDifficulty = 1;
+    // const gameActive = false;
+    // const gameConcluded = false;
+    // const timeRemaining = 60;
+
+
+
+
+
+
+}
+
+
+
+
+
+
+// gopher hit
+
+function gopherHit (){
+    // increase player points
+    playerScore = playerScore + 1
+
+    // hould find an audio file to add also...
+    // ..........
+
+    // update scoreboard
+    document.getElementById('player-score').innerText = playerScore; 
+}
+
+
+
+
+
+
+
+
+// golfer hit
+
+function golferHit (){
+    // point penalty
+    playerScore = playerScore - 10
+
+    //should find an audio file to add also...
+    // ..........
+
+    // update scoreboard
+    document.getElementById('player-score').innerText = playerScore; 
+}
+
+
+
+
+
+
+
+
+
 
 // example
 
@@ -401,34 +513,18 @@ document.getElementById('game-box').style.cursor = 'imgs/hammercursor.png';
 
 
 
-const carlSpacklerPoints = {}
+// const carlSpacklerPoints = {}
 
-const gopherPoints = {}
-
-
-
-function whackGopher () {}
-
-function whackGolfer () {}
+// const gopherPoints = {}
 
 
 
+// function whackGopher () {}
+
+// function whackGolfer () {}
 
 
 
-
-
-
-
-
-////////// ___ //////////
-
-
-
-
-
-
-////////// ___ //////////
 
 
 
