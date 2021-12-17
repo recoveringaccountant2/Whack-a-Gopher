838c65601f1825da91fd7026b26bf162a4a51839
Your Name <charlesjohnstoncpa@gmail.com>
2021-12-16 18:17:40 -0700

    mvp

--- js/main.js
+++ js/main.js
@@ -69,20 +69,11 @@ function screenStyleTransitionOneTwo() {
     // display name input field
     document.getElementById('player-name').style.display = 'inline-block';   
     // display button
-    document.getElementById('begin').style.display = 'inline-block';   
+    document.getElementById('begin').style.display = 'block';   
     // hide golf course background 
     document.getElementById('body').style.backgroundImage = 'none';
 }
     
-
-
-
-
-
-
-
-
-
 
 ////////// screen 2 //////////
 
@@ -105,8 +96,6 @@ const character = [
 
 let characterSelected = null
 
-
-
 function clearCharacterSelection() {
     // clear frame from any previously selected characters
     document.getElementById('ty').style.border = 'none';
@@ -117,7 +106,6 @@ function clearCharacterSelection() {
     document.getElementById('carl').style.border = 'none';
     document.getElementById('mrssmails').style.border = 'none';
     document.getElementById('danny').style.border = 'none';
-
     // turn off and reset any previous audio (except theme song)
     likeYouBetty.pause();
     likeYouBetty.currentTime = 0;
@@ -135,28 +123,7 @@ function clearCharacterSelection() {
     thankYouVeryLittle.currentTime = 0;
     beTheBall.pause();
     beTheBall.currentTime = 0;
-
-    // clear border
-
-    // document.getElementByClassName('character').style.border = 'none';
-    // document.getElementsByClassName('character').style.border = 'none';
-    // document.querySelector('character').style.border = 'none';
-    // document.querySelectorAll('character').style.border = 'none';
-
-
 }
-
-
-
-
-
-
-
-
-
-
-
-
 
 // event listener for Ty
 document.getElementById('ty').addEventListener('click', selectTy);
@@ -284,73 +251,38 @@ function initializeGame() {
 
 // style transition, hide, unhide elements
 function screenStyleTransitionTwoThree() {
+    // turn off and reset any previous audio (except theme song)
+    likeYouBetty.pause();
+    likeYouBetty.currentTime = 0;
+    youTakeDrugsDanny.pause();
+    youTakeDrugsDanny.currentTime = 0;
+    thanksForNothing.pause();
+    thanksForNothing.currentTime = 0;
+    hat.pause();
+    hat.currentTime = 0;
+    areYouMyPal.pause();
+    areYouMyPal.currentTime = 0;
+    gungaShort.pause();
+    gungaShort.currentTime = 0;
+    thankYouVeryLittle.pause();
+    thankYouVeryLittle.currentTime = 0;
+    beTheBall.pause();
+    beTheBall.currentTime = 0;
     // hide all screen 2 elements
     document.getElementById('screen-2').style.display = 'none';   
-
-
-
-
-
     document.getElementById('screen-3').style.display = 'grid';   
-
-
-
     document.getElementById('player-picture').style.display = 'block';
     document.getElementById('player-box').style.display = 'grid';
-
-
-
     document.getElementById('gopher-picture').style.display = 'block';
     document.getElementById('gopher-box').style.display = 'grid';
-
-
-
-    // display player score box
-    // document.getElementById('player-box').style.display = 'block';
-    // // display gopher score box
-    // document.getElementById('gopher-box').style.display = 'block';
-    // // display game field
-    // document.getElementById('game-box').style.display = 'grid';
-    // // display time remaining bar
-    // document.getElementById('time-remaining-bar').style.display = 'inline';   
-    // // display game controls
-    // document.getElementById('game-controls').style.display = 'inline';   
-
-
     document.getElementById('game-box').addEventListener('hover', hammerCursor);
-
-
-
-
-
     playerNameToScoreboard (playerName);
-
-
     document.getElementById('player-picture').src = character[characterSelected].photoUrl;
-
-
+    displayStartButton()
 }
-
-
-
-
-function hammerCursor (){
-document.getElementById('game-box').style.cursor = 'imgs/hammercursor.png';
-}
-
-
-
-
 
 
 ////////// screen 3 //////////
-
-
-
-
-
-
-
 
 // define game board variables 
 let playerScore = 0;
@@ -358,18 +290,7 @@ let gopherScore = 0;
 let gameDifficulty = 0;
 let gameActive = false;
 let gameOver = false;
-let timeRemaining = 60;
-
-// let results = [];
-
-// previously defined:
-// let playerName
-// const character
-
-
-
-
-
+let timeRemaining = 30;
 
 // display player name on game screen
 
@@ -380,45 +301,27 @@ function playerNameToScoreboard (){
 
 const difficultyOptions = ['slow poke', `lil' less embarrassing`, 'Speedy McClickerson', 'ludicrouse', 'plaid']
 
-
-// const gameDifficulty = 1;
-
-
 // event listener and function to set gameDifficulty
 document.getElementById('difficulty-input').addEventListener('click', updateDifficulty)
-
 
 function updateDifficulty (){
     gameDifficulty = document.getElementById('difficulty-input').value
     document.getElementById('difficulty-selected').innerText = difficultyOptions[gameDifficulty];
 }
 
-
 // difficulty settings
-
 let difficultySettings = [
+    {timeBetweenMin: '8000', timeBetweenMax: '16000', timeUpMin: "4000", timeUpMax: "8000"},
+    {timeBetweenMin: '4000', timeBetweenMax: '8000', timeUpMin: "2000", timeUpMax: "4000"},
     {timeBetweenMin: '2000', timeBetweenMax: '4000', timeUpMin: "1000", timeUpMax: "2000"},
-    {timeBetweenMin: '1500', timeBetweenMax: '3000', timeUpMin: "750", timeUpMax: "1500"},
     {timeBetweenMin: '1000', timeBetweenMax: '2000', timeUpMin: "500", timeUpMax: "1000"},
     {timeBetweenMin: '500', timeBetweenMax: '1000', timeUpMin: "250", timeUpMax: "500"},
-    {timeBetweenMin: '250', timeBetweenMax: '500', timeUpMin: "125", timeUpMax: "250"},
  ]
-
-
-
-
-// get gopher to display
-
-// .....
-
-
-
-
-// event listener to begin game
-document.getElementById('start-game').addEventListener('click', start)
 
 // begin game
 function start (){
+    // turn off event listener for start button
+    startButtons.forEach(startButton => startButton.removeEventListener('click', start));
     // zero out scores
     playerScore = 0;
     document.getElementById('player-score').innerText = playerScore; 
@@ -426,31 +329,28 @@ function start (){
     document.getElementById('gopher-score').innerText = gopherScore; 
     // reset game (for replays)
     gameOver = false;
-
+    golfersHit = 0;
+    gophersHit = 0;
+    gophersPresented = 0;
+    gophersPresented = 0;
     // stop kill gophers audio
     killGophers.pause();
-
     // turn up background music (on delay)
     setTimeout(function(){ 
         welcome.volume = 0.8;
     }, 2000);
-
     // play freeze gopher
     freezeGopher.play();
     freezeGopher.volume = .8;
-
     // small delay before game actually begins
     setTimeout(function(){ 
         gameActive = true;
-        timeRemaining = 60;
+        timeRemaining = 30;
         startTimer()
         displayGopher()
+        displayGolfer()
     }, 2500);
 }
-
-
-
-
 
 // timer functionality
 let timer;
@@ -470,55 +370,26 @@ function reduceTime(){
         clearInterval(timer);
         document.getElementById('time-remaining-value').innerText = 0;
         gameOver = true;
-//         displayResults()
+        gameActive = false;
+        setTimeout(function(){ 
+            displayStartButton();
+            timeRemaining = 30;
+            document.getElementById('time-remaining-value').innerText = timeRemaining;
+            // turn on event listener for start button
+            startButtons.forEach(startButton => startButton.addEventListener('click', start));
+        }, 4000);
     }
 }
 
-
-
-// gopher hit
-function gopherHit (){
-    // increase player points
-    playerScore = playerScore + 1
-    // stop prior punch and play new punch sound
-    punch.pause();
-    punch.currentTime = 0;
-    punch.play();
-    punch.volume = .6;
-    // update scoreboard
-    document.getElementById('player-score').innerText = playerScore; 
-}
-
-
-// golfer hit
-function golferHit (){
-    // point penalty
-    playerScore = playerScore - 10
-    // sound for golfer hit
-    nanaShort.play();
-    nanaShort.volume = 1;
-    // update scoreboard
-    document.getElementById('player-score').innerText = playerScore; 
-}
-
-
-
-
-
-
 const gophers = document.querySelectorAll('.gopher');
-
-// const playerNameEl = document.getElementById('player-name-scoreboard');
-
-
-
-
 
 // randomly generate a hole
 const holes = document.querySelectorAll('.hole');
 
 // to prevent duplicate hole selection
 let previousHole = null;
+
+let gophersPresented = 0;
 
 function randomHole (holes){
     const i = Math.floor(Math.random() * holes.length);
@@ -548,9 +419,11 @@ function timeBetween (){
 function displayGopher() {
     let timeDisplayed = timeUp();
     let hole = randomHole(holes);
-    hole.classList.add('displayed');
+    hole.classList.add('gopher-displayed');
+    gopherScore = gopherScore + 1;
+    document.getElementById('gopher-score').innerText = gopherScore; 
     setTimeout(() => {
-        hole.classList.remove('displayed');
+        hole.classList.remove('gopher-displayed');
             if(!gameOver) {
                 displayGopher();
             };
@@ -560,5 +433,78 @@ function displayGopher() {
 // event listener for gopher whacks
 gophers.forEach(gopher => gopher.addEventListener('click', gopherHit));
 
+// gopher hit
 
+gophersHit = 0;
+
+function gopherHit (){
+    // stop prior punch audio and play new one
+    punch.pause();
+    punch.currentTime = 0;
+    punch.play();
+    punch.volume = .6;
+    // update scoreboard
+    playerScore = playerScore + 1
+    document.getElementById('player-score').innerText = playerScore; 
+    gopherScore = gopherScore - 1;
+    document.getElementById('gopher-score').innerText = gopherScore; 
+    // lower hit gopher to prevent duplicate hits
+    document.querySelector('.gopher-displayed').classList.remove('gopher-displayed');
+}
+
+golfersPresented = 0;
+
+const golfers = document.querySelectorAll('.golfer');
+
+// display golfer
+function displayGolfer() {
+    let timeDisplayed = timeUp();
+    let hole = randomHole(holes);
+    hole.classList.add('golfer-displayed');
+    golfersPresented = golfersPresented + 1;
+    setTimeout(() => {
+        hole.classList.remove('golfer-displayed');
+            if(!gameOver) {
+                displayGolfer();
+            };
+    }, timeDisplayed);
+}
+
+// event listener for golfer whacks
+golfers.forEach(golfer => golfer.addEventListener('click', golferHit));
+
+let golfersHit = 0;
+
+// golfer hit
+function golferHit (){
+    // sound for golfer hit
+    nanaShort.play();
+    nanaShort.volume = 1;
+    // point penalty
+    playerScore = playerScore - 10
+    // tally golfer hits (needed for gopher score)
+    golfersHit = golfersHit + 1;
+    // update scoreboard
+    document.getElementById('player-score').innerText = playerScore; 
+    // lower hit golfer to prevent duplicate hits
+    document.querySelector('.golfer-displayed').classList.remove('golfer-displayed');
+}
+
+const startButtons = document.querySelectorAll('.start-button');
+
+// display start button
+function displayStartButton() {
+    let timeStartDisplayed = 1500;
+    let hole = randomHole(holes);
+    hole.classList.add('start-button-displayed');
+    setTimeout(() => {
+        hole.classList.remove('start-button-displayed');
+            if(!gameActive) {
+                displayStartButton();
+            };
+    }, timeStartDisplayed);
+}
+
+// event listener for start button
+startButtons.forEach(startButton => startButton.addEventListener('click', start));
 
